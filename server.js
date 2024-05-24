1df36b1ec10bc5fdba9d2b8a1ab2b9dece60291c
Guibson RAvelly <Guibsonravelly@gmail.com>
2024-05-23 21:33:56 -0300

    finalizado

--- server.js
+++ server.js
@@ -2,54 +2,29 @@
 const express = require("express")
 const server = express()
 
+const db = require("./db")
 
-const ideas= [
 
-    {
-        img:"/public/src/programacao.png",
-        title:"Trabalhos",
-        category:"Estudos",
-        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit",
-        url:"https://www.linkedin.com/in/guibson-ravelly-4a2700140/"
-    },
 
-    {
-        img:"/public/src/objetivo.png",
-        title:"Objetivo",
-        category:"Execuçao",
-        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit",
-        url:"https://www.linkedin.com/in/guibson-ravelly-4a2700140/"
-    },
 
-    {
-        img:"/public/src/pequisa.png",
-        title:"Resultado",
-        category:"perfomace",
-        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit",
-        url:"https://www.linkedin.com/in/guibson-ravelly-4a2700140/"
-    },
-
-    {
-        img:"/public/src/pesquisa.png",
-        title:"Resultado",
-        category:"perfomace",
-        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit",
-        url:"https://www.linkedin.com/in/guibson-ravelly-4a2700140/"
-    },
-
- 
-]
 
 
 //configura aquivos estaticos (css, scrip , imagens )
 
 server.use(express.static("public" ))
 
+
+// habilitar uso do req.body
+
+server.use(express.urlencoded({ extend:true }))
+
+
 // configuando nunjucks
 
 const nunjucks = require("nunjucks")
 nunjucks.configure("views", {
     express: server,
+    autoescape:false,
     noCache:true //boloean
 })
 
@@ -62,23 +37,75 @@ nunjucks.configure("views", {
 
 server.get("/",function(req, res) {
 
-    
-    return res.render("index.html", { ideas } )
+     db.all(`SELECT * FROM ideas`, function(err, rows){
+         if (err) return
+
+            const reverseIdeas = [...rows].reverse()
+            let lastIdeas =[]
+            for(let idea of reverseIdeas){
+                if(lastIdeas.length < 2){
+                    lastIdeas.push(idea)
+                }
+            }
+           
+            return res.render("index.html", {ideas: lastIdeas} )
+     })
+
 })
 
 server.get("/ideias",function(req, res) {
-    return res.render("ideias.html")
+
+    
+
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if (err) {
+            return res.send("Error no banco de dados")
+        }
+        const reverseIdeas = [...rows].reverse()
+    return res.render("ideias.html",{ideas:reverseIdeas  } )
+           
+    })
 })
 
+server.post("/", function(req, res){
 
+    const query =`
+     INSERT INTO ideas(
+        image,
+         titlle,
+         category,
+         description,
+         link
+      ) VALUES (?,?,?,?,?)`
 
+      const values =  [
+         req.body.image,
+         req.body.title,
+         req.body.category,
+         req.body.description,
+         req.body.link,
+     ]
 
+      db.run( query, values , function(err){
+        if (err) {
+            return res.send("Error no banco de dados")
+        }
 
+         return res.redirect("/ideias")
+
+      })
+
+})
 
 
 
 
 // liguei meu servidor na porta 3000
-server.listen(3000)
+server.listen(3000 , function(){
+    console.log("Boa GUIBSON")
+    
+})
+
+
 
 
